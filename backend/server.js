22826b84a94440fc7468c0c360cb4a2ebd199646
Kisaja Beddawala <kisajab72@gmail.com>
2026-01-29 15:47:31 +0530

    setup frontend part

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ app.use(cors());
 app.use(express.json());
 
 db();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
 })
