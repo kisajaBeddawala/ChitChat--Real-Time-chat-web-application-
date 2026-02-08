7e5c8547914617e59ac8535f22a09745bea389a8
Kisaja Beddawala <kisajab72@gmail.com>
2026-02-08 10:29:35 +0530

    finish user routes, models and controllers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const server = http.createServer(app)
@@ -11,7 +12,9 @@ app.use(express.json({limit:'4mb'}))
 app.use(cors())
 
 app.use("/api/status", (req,res) => res.send("server is live"))
+app.use("/api/auth",userRouter);
 
+// connect to MongoDB
 await connectDB();
 
 const PORT = process.env.PORT || 5000
