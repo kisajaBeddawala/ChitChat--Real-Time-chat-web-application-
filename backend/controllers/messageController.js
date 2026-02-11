f1543dd062e9a2a70c0612fa88803dacb5052dfe
Kisaja Beddawala <kisajab72@gmail.com>
2026-02-11 19:40:36 +0530

    create message model. create auth context

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,5 +1,7 @@
-import Message from "../models/Message";
-import User from "../models/User";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 // get all users except logged in user
 export const getUsersForSidebar = async(req,res) => {
@@ -49,6 +51,38 @@ export const markMessageAsSeen = async (req,res) => {
         const {id} = req.params;
         await Message.findByIdAndUpdate(id, {seen:true});
         res.json({success:true})
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false, message:error.message});
+    }
+}
+
+// send message to selected user
+export const sendMessage = async (req,res) => {
+    try{
+        const { text, image} = req.body;
+        const receiverId = req.params.id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        // emit message to receiver if online
+        const receiverSocketId = userSocketMap[receiverId];
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage);
+        }
+        res.json({success:true, message:newMessage});
     }catch(error){
         console.log(error.message);
         res.json({success:false, message:error.message});
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,36 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 const app = express();
 const server = http.createServer(app)
+
+// socket io setup
+export const io = new Server(server, {
+    cors:{
+        origin: "*",
+    }
+});
+
+// store online users
+export const userSocketMap = {}
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("New client connected: ", userId);
+
+    if(userId){
+        userSocketMap[userId] = socket.id;
+    }
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected: ", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
+})
 
 app.use(express.json({limit:'4mb'}))
 app.use(cors())
